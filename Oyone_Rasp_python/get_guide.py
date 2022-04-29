3a22fc05be7468d06f12524b7dd1fe34f6c0be14
Kentaro-Machida <kenkenm120319@gmail.com>
2022-04-29 09:20:50 +0900

    Added error value to the guideline

--- Oyone_Rasp_python/get_guide.py
+++ Oyone_Rasp_python/get_guide.py
@@ -17,28 +17,38 @@ class Guide_getter():
         self.rho = 2
         self.theta = np.pi/180
         self.threshold = 30
-        self.mini_line_length = 10
+        self.mini_line_length = 5
 
         # perspective transform parammeters
         # [top_left, top_right, lower_left, lower_right]
         self.src_coordinate = np.float32([(200, 200), (420, 200), (0, 280), (620, 280)])
         self.dst_coordinate = np.float32([(0, 400), (400, 400), (0, 0), (400, 0)])
         self.bird_view_size = (400, 400)
+        self.center_index = int(self.bird_view_size[0]/2)
 
         # ROI(Region of Interest) top coordinate
         self.ROI_top = 200
+
+        # initialize index value
+        # -1 is error number
+        self.left_index = -1
+        self.right_index = -1
+        self.guideline_index = -1
+        
 
     """
     set frame to get guide line
     input
     frame: np.ndarray
     """
-    def set_frame(self, frame) -> None:
+    def set_frame(self, frame:np.ndarray) -> None:
         self.frame = frame
 
     """
-    Get guide line index of bird's eye frame
-    edges_img, bgr_bird_line_img are instance variables
+    input: None
+    return: 
+    guide line is detected -> guide line(int)
+    guide line isn't detected -> -1(int)
     """
     def get_guide_line(self)->int:
         histogramLane = []        
@@ -51,9 +61,10 @@ class Guide_getter():
         lines = cv2.HoughLinesP(self.edges_img, self.rho, self.theta, self.threshold, minLineLength=self.mini_line_length)
 
         # write detected line to frame
-        for line in lines:
-            for x1,y1,x2,y2 in line:
-                cv2.line(self.bgr_bird_line_img, (x1,y1),(x2,y2),(255, 0, 0),2)
+        if (lines is not None):
+            for line in lines:
+                for x1,y1,x2,y2 in line:
+                    cv2.line(self.bgr_bird_line_img, (x1,y1),(x2,y2),(255, 0, 0),2)
 
         # transform input frame which line written to bird's eye frame
         trans_mat = cv2.getPerspectiveTransform(self.src_coordinate, self.dst_coordinate)
@@ -68,15 +79,26 @@ class Guide_getter():
             ROILane = np.sum(gray_bird_line_img[self.ROI_top:,i])
             histogramLane.append(ROILane)
 
-        center_index = int(self.bird_view_size[0]/2)
-        left_index = np.argmax(histogramLane[:center_index])
-        right_index = np.argmax(histogramLane[center_index:]) + center_index
-        guideline_index = int((right_index - left_index)/2 + left_index)
+        self.left_index = np.argmax(histogramLane[:self.center_index])
+        self.right_index = np.argmax(histogramLane[self.center_index:]) + self.center_index
+        left_side_max = np.max(histogramLane[self.center_index:])
+        right_side_max = histogramLane[self.center_index:]
+        if (left_side_max == 0 or right_side_max == 0):
+            self.guideline_index = -1
+        else:
+            self.guideline_index = int((self.right_index - self.left_index)/2 + self.left_index)
 
         # write right, left, center guide lines
-        cv2.line(self.bgr_bird_line_img,(guideline_index,0),(guideline_index,self.bird_view_size[1]),(0,0,255),2)
-        cv2.line(self.bgr_bird_line_img,(center_index,0),(center_index,self.bird_view_size[1]),(255,0,255),2)
-        return guideline_index
+        cv2.line(self.bgr_bird_line_img,(self.guideline_index,0),(self.guideline_index,self.bird_view_size[1]),(0,0,255),2)
+        cv2.line(self.bgr_bird_line_img,(self.center_index,0),(self.center_index,self.bird_view_size[1]),(255,0,255),2)
+        return self.guideline_index
+
+    def get_center_index(self) -> int:
+        return self.center_index
+
+    def get_LR_index(self) -> tuple:
+        return (self.left_index,self.right_index)
+
 
     # get edge frame
     def get_edges_frame(self) -> np.ndarray:
@@ -103,6 +125,10 @@ if __name__=='__main__':
 
     guide_line_index = test_getter.get_guide_line()
     guide_frame = test_getter.get_final_frame()
+    LR = test_getter.get_LR_index()
+
+    print("guide line: ",guide_line_index)
+    print("(Left, Right)",LR)
 
     cv2.imshow('test',guide_frame)
     cv2.waitKey(0)
