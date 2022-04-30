62222498c33baf7020b88cbe154211448272d3b7
oyone Raspberry Pi <kenkenm120319@gmail.com>
2022-04-30 17:28:22 +0900

    Added a function to get direction signal from arduino.

--- Oyone_Rasp_python/serial_communication.py
+++ Oyone_Rasp_python/serial_communication.py
@@ -1,8 +1,13 @@
 import serial
+import time
 
 class communicator():
-    def __init__(self,port="/dev/ttyACAM0",baudrate=9600,timeout=0.1):
+    def __init__(self,port="/dev/ttyACM0",baudrate=9600,timeout=0.1):
         self.serial_communicator = serial.Serial(port, baudrate, timeout=timeout)
         
-    def serial_wirte(self,x:str):
-        self.serial_communicator.write(bytes(x,'utf-8'))
+    def serial_wirte(self,x:str,delay=0):
+        time.sleep(delay)
+        self.serial_communicator.write(bytes(x,'UTF-8'))
+
+    def serial_read(self, until='\n')->str:
+        return self.serial_communicator.read_until(until)
