53349ef1007129d3a20c23f3fc45ce68d9a632d2
Kentaro-Machida <kenkenm120319@gmail.com>
2022-05-01 10:25:15 +0900

    added manual mode and mode selection function.

--- Oyone_Rasp_python/main.py
+++ Oyone_Rasp_python/main.py
@@ -1,16 +1,26 @@
 """
-Shoot a movie with the camera and determine the direction oyone should take
+Take a video from the camera and run oyone.
+You can choose from three driving modes.
+1. auto run 
+2. auto run while outputting photos.
+3. manual run: you can operate the ship with the arrow keys.
+ex) 
+python main.py auto
+python main.py auto_with_image
+python main.py manual
 """
 
+import sys
 import cv2
 import get_guide
 import serial_communication
 import direction
+from manual import manual_direction
 
 cap = cv2.VideoCapture(0)
 guide_line_getter = get_guide.Guide_getter()
 
-def run_only():
+def autorun():
     to_arduino = serial_communication.communicator()
     while True:
         ret, frame = cap.read()
@@ -35,7 +45,7 @@ def run_only():
             if key == 27:
                 break
 
-def run_with_image():
+def autorun_with_image():
     to_arduino = serial_communication.communicator()
     while True:
         ret, frame = cap.read()
@@ -67,8 +77,55 @@ def run_with_image():
             if key == 27:
                 break
         
-    cap.release()
-    cv2.destroyAllWindows()
+def manual_run():
+    to_arduino = serial_communication.communicator()
+    while True:
+        ret, frame = cap.read()
+        guide_line_getter.set_frame(frame)
 
-run_with_image()
+        if ret == False:
+            print('Camera is not detected.')
+            break
+        else:
+            edge_img = guide_line_getter.get_edges_frame()
+            final = guide_line_getter.get_final_frame()
 
+            cv2.imshow('input image',frame)
+            cv2.imshow('edge image', edge_img)
+            cv2.imshow('final image',final)
+
+            key = cv2.waitKey(1)
+            which = manual_direction(key)
+            to_arduino.serial_wirte(which)
+
+            rtn_which = to_arduino.serial_read()
+            print(rtn_which)
+            
+            # key27: esc
+            if key == 27:
+                break
+
+def main()->None:
+    args = sys.argv
+    if len(args)<1:
+        print('Please select a mode.\n \"auto\" or \"manual\" or \"auto_with_image\".')
+    elif len(args)>2:
+        print('Enter only one argument.\n \"auto\" or \"manual\" or \"auto_with_image\".')
+    else:
+        if(args[1]=='manual'):
+            print('manual mode start.\n')
+            manual_run()
+        elif(args[1]=='auto_with_image'):
+            print('auto mode(output image) start.\n')
+            autorun_with_image()
+        elif(args[1]=='auto'):
+            print('auto mode start.\n')
+            autorun()
+        else:
+            print(args[1],"mode is not exit.")
+
+if __name__=='__main__':
+    main()
+
+cap.release()
+cv2.destroyAllWindows()
