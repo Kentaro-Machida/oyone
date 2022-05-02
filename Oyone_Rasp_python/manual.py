84984fc327d47cb5de2d83b2420c55a11f37bd71
oyone Raspberry Pi <kenkenm120319@gmail.com>
2022-05-02 11:38:17 +0900

    Added compatibility with external keyboards

--- Oyone_Rasp_python/manual.py
+++ Oyone_Rasp_python/manual.py
@@ -2,11 +2,17 @@
 Code for manual operation.
 """
 
-# arrows key value of opencv waitkey
-up=0
-down=1
-left=2
-right=3
+# arrows key value of opencv waitkey (MacOS)
+#up=0
+#down=1
+#left=2
+#right=3
+
+# arrows key value of opencv waitkey (external keyboard)
+up=82
+down=84
+left=81
+right=83
 
 # Convert from arrow keys to characters required by arduino
 def manual_direction(key:int) -> str:
