8ab5f54b9737a781643e4c86fb0b24ab3c71d75b
Lab Ubuntu <kenkenm120319@gmail.com>
2022-07-08 16:15:46 +0900

    [new]Emergency code

---
+++ Oyone_Rasp_python/camera_only.py
@@ -0,0 +1,16 @@
+"""
+oyoneによるデータ収集の非常用ファイル
+もし、モーターおよびArduinoがだめになったとしても
+カメラとラズパイさえ生きていれば撮影ができるようにする
+"""
+
+import cv2
+import save_sensor_img
+
+data_saver = save_sensor_img(save_video=True, save_sensor=True)
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    key = cv2.waitKey(1)
+    sensor_dict = None
+    data_saver.save(frame, sensor_dict, '-')
